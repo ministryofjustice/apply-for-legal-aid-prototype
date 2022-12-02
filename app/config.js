aceb2ccd32b547e8835d04c22899f12dfb6d790c
reyrrep <justin.perryer@digital.justice.gov.uk>
2022-12-02 13:28:16 +0000

    home page and service name update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for legal aid',
+  serviceName: 'Apply for civil legal aid',
 
   // Default port that prototype runs on
   port: '3000',
