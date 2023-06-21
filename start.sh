9b072cc92fde1cdb5bb880376ba4aff4b8a19e45
justin perryer <justin.perryer@digital.justice.gov.uk>
2023-06-21 09:41:39 +0100

    start file amend

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 # maybe replace 5 and 6 with - npm run serve
-node ./node_modules/gulp/bin/gulp generate-assets \
-  && node listen-on-port.js
+npm run serve
+# node ./node_modules/gulp/bin/gulp generate-assets \
+#   && node listen-on-port.js
